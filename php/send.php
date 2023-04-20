148f50ea77a254b87033dfb329786a0481937222
4jawaly.com <120571011+4jawalycom@users.noreply.github.com>
2023-04-20 10:20:49 +0200

    Update send.php

--- php/send.php
+++ php/send.php
@@ -1,31 +1,50 @@
 <?php
-$curl = curl_init();
-$app_id = "x";
-$app_sec = "y";
-$app_hash  = base64_encode("$app_id:$app_sec");
-$messages = [];
-$messages["messages"] = [];
-$messages["messages"][0]["text"] = "test php";
-$messages["messages"][0]["numbers"][] = "966xxxx";
-$messages["messages"][0]["sender"] = "test";
+$apiKey = "api key";
+$apiSecret = "api secret";
+$appHash = base64_encode("$apiKey:$apiSecret");
 
-curl_setopt_array($curl, array(
-    CURLOPT_URL => 'https://api-sms.4jawaly.com/api/v1/account/area/sms/send',
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => '',
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 0,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => 'POST',
-    CURLOPT_POSTFIELDS =>json_encode($messages),
-    CURLOPT_HTTPHEADER => array(
-        'Accept: application/json',
-        'Content-Type: application/json',
-        'Authorization: Basic '.$app_hash
-    ),
-));
+$messages = array(
+    "messages" => array(
+        array(
+            "text" => "test",
+            "numbers" => array("9665000000"),
+            "sender" => "4jawaly"
+        )
+    )
+);
 
-$response = curl_exec($curl);
-curl_close($curl);
-var_dump(json_decode($response));
+$url = "https://api-sms.4jawaly.com/api/v1/account/area/sms/send";
+$headers = array(
+    "Accept: application/json",
+    "Content-Type: application/json",
+    "Authorization: Basic $appHash"
+);
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($messages));
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$response = curl_exec($ch);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+curl_close($ch);
+
+if ($httpCode == 200) {
+    $responseJson = json_decode($response, true);
+    if (isset($responseJson["messages"][0]["err_text"])) {
+        echo $responseJson["messages"][0]["err_text"];
+    } else {
+        echo "تم الارسال بنجاح  " . " job id:" . $responseJson["job_id"];
+    }
+} elseif ($httpCode == 400) {
+    $responseJson = json_decode($response, true);
+    echo $responseJson["message"];
+} elseif ($httpCode == 422) {
+    echo "نص الرسالة فارغ";
+} else {
+    echo "محظور بواسطة كلاودفلير. Status code: $httpCode";
+}
+?>
