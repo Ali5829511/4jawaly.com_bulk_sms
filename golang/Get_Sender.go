b204329a029bcc4425dacd8115faf9af5880024a
4jawaly.com <120571011+4jawalycom@users.noreply.github.com>
2023-04-21 04:42:07 +0200

    Update Get_Sender.go

--- golang/Get_Sender.go
+++ golang/Get_Sender.go
@@ -1,76 +1,29 @@
-package main
+import requests
+import json
+import base64
 
-import (
-	"encoding/base64"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"strings"
-)
+app_id = "Api Key"
+app_sec = "Api Secret"
+app_hash = base64.b64encode(f"{app_id}:{app_sec}".encode()).decode()
+base_url = "https://api-sms.4jawaly.com/api/v1/"
 
-func main() {
-	appID := "Api key"
-	appSec := "Api Secret"
-	appHash := base64.StdEncoding.EncodeToString([]byte(appID + ":" + appSec))
-	baseURL := "https://api-sms.4jawaly.com/api/v1/"
+query = {}  # Define the query parameters here if needed
 
-	query := make(map[string]string) // Define the query parameters here if needed
+url = base_url + "account/area/senders?" + "&".join(f"{k}={v}" for k, v in query.items())
 
-	url := baseURL + "account/area/senders?"
-	for k, v := range query {
-		url += fmt.Sprintf("%s=%s&", k, v)
-	}
-	url = strings.TrimRight(url, "&")
-
-	headers := map[string]string{
-		"Accept":        "application/json",
-		"Content-Type":  "application/json",
-		"Authorization": "Basic " + appHash,
-	}
-
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		fmt.Println("Error creating request:", err)
-		return
-	}
-
-	for k, v := range headers {
-		req.Header.Set(k, v)
-	}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("Error executing request:", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Error reading response body:", err)
-		return
-	}
-
-	responseJSON := make(map[string]interface{})
-	err = json.Unmarshal(body, &responseJSON)
-	if err != nil {
-		fmt.Println("Error unmarshalling response JSON:", err)
-		return
-	}
-
-	code := int(responseJSON["code"].(float64))
-	fmt.Println("error code:", code)
-
-	if code == 200 {
-		itemsInterface := responseJSON["items"].(map[string]interface{})
-		data := itemsInterface["data"].([]interface{})
-		for _, item := range data {
-			itemMap := item.(map[string]interface{})
-			fmt.Println(itemMap["sender_name"].(string))
-		}
-	} else {
-		fmt.Println("message:", responseJSON["message"].(string))
-	}
+headers = {
+    "Accept": "application/json",
+    "Content-Type": "application/json",
+    "Authorization": f"Basic {app_hash}"
 }
+
+response = requests.get(url, headers=headers)
+response_json = json.loads(response.text)
+
+print("error code:", response_json["code"])
+
+if response_json["code"] == 200:
+    for item in response_json["items"]["data"]:
+        print(item["sender_name"])
+else:
+    print("message:", response_json["message"])
